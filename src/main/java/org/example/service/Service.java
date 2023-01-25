3b2dd6b0db16d7c807c6b7c5dac260c59bd7a288
Josep SS <josepsalgado@hotmail.com>
2023-01-25 14:09:26 +0100

    JOSEP: Commit per guardar les modificacions

--- src/main/java/org/example/service/Service.java
+++ src/main/java/org/example/service/Service.java
@@ -12,254 +12,14 @@ public class Service implements Serv {
 
     //region MAIN METHODS
 
-    /**
-     * Method to get all products there is on DDBB
-     * @return List of all products
-     */
-    @Override
-    public List<Product> getAllProducts() {
-        //region DEFINITION VARIABLES
-        List<Product> productList = null;
-        Repository repoCls;
 
-        //endregion DEFINITION VARIABLES
 
 
-        //region ACTIONS
-        try{
-            // INIT VARIABLES
-            repoCls = new Repository();
-            productList = new ArrayList<Product>();
 
-            // CALL REPOSITORY METHOD
-            productList.addAll(repoCls.getAllProducts());
+    //endregion MAIN METHODS
 
-        }catch (Exception ex) {
-            //TODO control errors
-        }
 
-        //endregion ACTIONS
-
-
-        // OUT
-        return productList;
-
-    }
-
-    @Override
-    public List<Ticket> getAllSells() {
-        //region DEFINITION VARIABLES
-        List<Ticket> sellList = null;
-        Repository repoCls;
-
-        //endregion DEFINITION VARIABLES
-
-
-        //region ACTIONS
-        try{
-            // INIT VARIABLES
-            repoCls = new Repository();
-            sellList = new ArrayList<Ticket>();
-
-
-            // CALL REPOSITORY METHOD
-            sellList.addAll(repoCls.getAllSells());
-
-        }catch(Exception ex) {
-            //TODO control errors
-        }
-        //endregion ACTIONS
-
-
-        // OUT
-       return sellList;
-    }
-
-    @Override
-    public void createProduct(Product product) {
-        //region DEFINITION VARIABLES
-        Repository repoCls;
-
-        //endregion DEFINITION VARIABLES
-
-
-        //region ACTIONS
-        try{
-            // INIT VARIABLES
-            repoCls = new Repository();
-
-            // CALL REPOSITORY METHODS
-            if(product.getClass() == Decoration.class){
-                repoCls.createDeco((Decoration) product);
-            }else if(product.getClass() == Flower.class){
-                repoCls.createFlower((Flower) product);
-            }else if(product.getClass() == Tree.class){
-                repoCls.createTree((Tree) product);
-            }else{
-                //TODO Llançar error que la classe no es correcte
-            }
-
-
-        }catch(Exception ex){
-            //TODO control errors
-        }
-
-        //endregion ACTIONS
-
-
-        // OUT
-
-
-    }
-
-    @Override
-    public void createTicket(Ticket ticket) {
-        //region DEFINITION VARIABLES
-        Double totalPrice;
-        List<ProductforSale> productsList;
-        Repository repoCls;
-
-        //endregion DEFINITION VARIABLES
-
-
-        //region ACTIONS
-        try{
-            // INIT VARIABLES
-            productsList = new ArrayList<>();
-            repoCls = new Repository();
-
-            // 1) CALCULATE TOTAL PRICE
-            productsList.addAll(ticket.getProductforSales());
-            totalPrice = productsList.stream().mapToDouble(p-> p.getPrice()).sum();
-
-            // 2) SAVE SELL
-//            sell.
-//            repoCls.createTicket();
-
-            // 3) SAVE TICKET'S PRODUCTS
-
-
-            // CALL REPOSITORY METHOD
-            repoCls.createTicket(ticket);
-
-        }catch(Exception ex){
-            //TODO control erros
-        }
-
-        //endregion ACTIONS
-
-
-        // OUT
-
-    }
-
-    @Override
-    public void createSell(Ticket ticket) {
-        //region DEFINITION VARIABLES
-        Repository repoCls;
-
-        //endregion DEFINITION VARIABLES
-
-
-        //region ACTIONS
-        try{
-            // INIT VARIABLES
-            repoCls = new Repository();
-
-            // CALL REPOSITORY METHOD
-
-            repoCls.createSell(ticket);
-
-
-        }catch(Exception ex){
-            //TODO control errors
-        }
-
-        //endregion ACTIONS
-
-
-        // OUT
-
-
-    }
-
-    /**
-     * Get total value of sales
-     * @return Double of value.
-     */
-    @Override
-    public double sumSales() {
-        //region DEFINITION VARIABLES
-        List<Ticket> sellList = new ArrayList<Ticket>();
-        Repository repoCls;
-
-
-        //endregion DEFINITION VARIABLES
-
-
-        //region ACTIONS
-        try {
-            // INIT VARIABLES
-            repoCls = new Repository();
-
-            // GET LIST OF SALES
-            sellList.addAll(repoCls.getAllSells());
-
-            for (Ticket s: sellList) {
-
-            }
-
-        } catch (Exception ex) {
-            //TODO control errors
-        }
-        //endregion ACTIONS
-
-
-        // OUT
-
-
-        return 0;
-    }
-
-    @Override
-    public double sumSell() {
-        return 0;
-    }
-
-    @Override
-    public double sumStock() {
-        return 0;
-    }
-
-    @Override
-    public String init() {
-        //region DEFINITION VARIABLES
-        String name;
-        Repository repoCls;
-
-        //endregion DEFINITION VARIABLES
-
-
-        //region ACTIONS
-        try {
-            // INIT VARIABLES
-            repoCls = new Repository();
-            name="";
-
-            // CALL REPOSITORY METHOD
-            name = repoCls.init();
-
-
-        } catch (Exception ex) {
-            throw ex;
-        }
-        //endregion ACTIONS
-
-
-        // OUT
-        return name;
-    }
-
+    //region METHODS: CREATE
     @Override
     public void createFlowerShop(String name) {
         //region DEFINITION VARIABLES
@@ -277,48 +37,246 @@ public class Service implements Serv {
             repoCls.createFlowerShop(name);
 
         } catch (Exception ex) {
-            throw ex;
+            //TODO control errors
         }
 
         //endregion ACTIONS
 
     }
 
-    //endregion MAIN METHODS
-
-
-    //region AUXILARY METHODS
-    private double sumSellAux(Ticket sell){
+    /**
+     * Metode per crear un nou tipus de producte, de qualsevol mena.
+     * @param product Necessita una classe del tipus de producte que s'ha de crear.
+     * @return false=> No s'ha pogut crear el producte; true => s'ha creat el producte correctament
+     */
+    @Override
+    public boolean createProduct(Product product) {
         //region DEFINITION VARIABLES
-        double totalValue = 0;
-        List<Ticket> ticketList;
+        boolean resul = false;
         Repository repoCls;
-
 
         //endregion DEFINITION VARIABLES
 
 
+        //region ACTIONS
         try {
-            //region ACTIONS
             // INIT VARIABLES
             repoCls = new Repository();
-            ticketList = new ArrayList<>();
-            //ticketList.addAll(sercCls.)
 
+            // CALL REPOSITORY METHODS
+            if (product.getClass() == Decoration.class) {
+                repoCls.createDeco((Decoration) product);
+            } else if (product.getClass() == Flower.class) {
+                repoCls.createFlower((Flower) product);
+            } else if (product.getClass() == Tree.class) {
+                repoCls.createTree((Tree) product);
+            } else {
+                //TODO Llançar error que la classe no es correcte
+            }
 
-            //endregion ACTIONS
-
-
-            // OUT
-            return totalValue;
-        }catch(Exception ex){
-            throw ex;
+            resul = true;
+        } catch (Exception ex) {
+            //TODO control errors
+            resul = false;
         }
 
+        //endregion ACTIONS
+
+
+        // OUT
+        return resul;
 
     }
 
+    @Override
+    public boolean createTicket(Ticket ticket) {
+        //region DEFINITION VARIABLES
+        boolean result =false;
+        Double totalPrice= 0.0;
+        List<ProductforSale> productsList;
+        Repository repoCls;
+
+        //endregion DEFINITION VARIABLES
+
+
+        //region ACTIONS
+        try {
+            // INIT VARIABLES
+            productsList = new ArrayList<>();
+            repoCls = new Repository();
+
+            // 1) CALCULATE TOTAL PRICE
+            productsList.addAll(ticket.getProductforSales());
+            ticket.setTotalPrice(productsList.stream().mapToDouble(p -> p.getPrice()).sum());
+
+            // 2) SAVE TICKET
+            repoCls.createTicket(ticket);
+
+            result = true;
+        } catch (Exception ex) {
+            //TODO control erros
+            result = false;
+        }
+
+        //endregion ACTIONS
+
+
+        // OUT
+        return result;
+
+    }
+
+    //TODO eliminar aquest mètode
+//    @Override
+//    public void createSell(Ticket ticket) {
+//        //region DEFINITION VARIABLES
+//        Repository repoCls;
+//
+//        //endregion DEFINITION VARIABLES
+//
+//
+//        //region ACTIONS
+//        try {
+//            // INIT VARIABLES
+//            repoCls = new Repository();
+//
+//            // CALL REPOSITORY METHOD
+//
+//            repoCls.createSell(ticket);
+//
+//
+//        } catch (Exception ex) {
+//            //TODO control errors
+//        }
+//
+//        //endregion ACTIONS
+//
+//
+//        // OUT
+//
+//
+//    }
+
+    //endregion METHODS: CREATE
+
+
+    //region METHODS:GET
+
+    /**
+     * Method to get all products there is on DDBB
+     *
+     * @return List of all products
+     */
+    @Override
+    public List<Product> getAllProducts() {
+        //region DEFINITION VARIABLES
+        List<Product> productList = null;
+        Repository repoCls;
+
+        //endregion DEFINITION VARIABLES
+
+
+        //region ACTIONS
+        try {
+            // INIT VARIABLES
+            repoCls = new Repository();
+            productList = new ArrayList<Product>();
+
+            // CALL REPOSITORY METHOD
+            productList.addAll(repoCls.getAllProducts());
+
+        } catch (Exception ex) {
+            //TODO control errors
+        }
+
+        //endregion ACTIONS
+
+
+        // OUT
+        return productList;
+
+    }
+
+    //endregion METHODS: GET
+
+
+    //region METHODS: UPDATE
+
+    /**
+     * Mètode per actualitzar la info d'un producte.
+     * @param product Necessita la classe del producte que s'ha de modificar.
+     * @return false = hi hagut algun error; true = tot ha sortit bé.
+     */
+    @Override
+    public boolean updateProduct(Product product) {
+        return false;
+    }
+    //endregion METHODS: UPDATE
+
+
+    //region METHODS: OTHERS (INIT, SUM,...)
+    @Override
+    public String init() {
+        //region DEFINITION VARIABLES
+        String name = "";
+        Repository repoCls;
+
+        //endregion DEFINITION VARIABLES
+
+
+        //region ACTIONS
+        try {
+            // INIT VARIABLES
+            repoCls = new Repository();
+
+            // CALL REPOSITORY METHOD
+            name = repoCls.init();
+
+        } catch (Exception ex) {
+            //TODO control errors
+        }
+        //endregion ACTIONS
+
+
+        // OUT
+        return name;
+    }
+
+    /**
+     * Mètode per sumar el valor de tot el stock de la floristeria.
+     * @return el valor de la suma
+     */
+    @Override
+    public double sumStock() {
+        return 0;
+    }
+
+    /**
+     * Mètode per sumar el valor de tots els productesque hi ha en el ticket.
+     * @return el valor del tiquet.
+     */
+    @Override
+    public double sumTicket(Ticket ticket) {
+        return 0;
+    }
+
+    /**
+     * Mètode per sumar el valor de tots els tickets que s'han creat
+     * @return el valor de la suma
+     */
+    @Override
+    public double sumAllTickets() {
+        return 0;
+    }
+    //endregion METHODS: OTHERS (SUM,...)
+
+
+    //region AUXILARY METHODS
+
+
     //endregion AUXILARY METHODS
+
+
 
 }
 
